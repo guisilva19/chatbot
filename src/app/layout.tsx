1298195845a39676e434cbb11d412a7cd5ab2ad4
Guilherme Silva <2003silvagui@gmail.com>
2024-04-08 16:18:46 -0300

    feat: icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: "Chatbot",
   description:
     "O projeto de desenvolvimento de um chatbot utilizando a API da OpenAI é uma iniciativa emocionante. Com a API da OpenAI, temos acesso a uma tecnologia avançada de processamento de linguagem natural, permitindo ao chatbot entender e gerar textos de forma fluente e relevante. ",
+    icons: {
+      icon: "./icon.png"
+    }
 };
 
 export default function RootLayout({
