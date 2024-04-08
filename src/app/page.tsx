352a93ffe7f23d356558aa640bb09b88fc568172
Guilherme Silva <2003silvagui@gmail.com>
2024-04-08 16:33:22 -0300

    feat: config nextui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { NextUIProvider } from "@nextui-org/react";
+
 export default function Home() {
   return (
-    <>
+    <NextUIProvider>
       <main className="w-screen h-screen flex justify-center items-center bg-sky-50">
         <div className="w-[375px] h-[650px] rounded-lg border-2 border-slate-400 bg-white"></div>
       </main>
-    </>
+    </NextUIProvider>
   );
 }
