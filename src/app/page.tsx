71ac4178a2bb8fdddf065ed0521d93a76255bd6b
Guilherme Silva <2003silvagui@gmail.com>
2024-04-08 16:25:27 -0300

    feat: custom layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <main className=""></main>
+      <main className="w-screen h-screen flex justify-center items-center bg-sky-50">
+        <div className="w-[375px] h-[650px] rounded-lg border-2 border-slate-400 bg-white"></div>
+      </main>
     </>
   );
 }
